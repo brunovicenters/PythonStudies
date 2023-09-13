26ae52fbb335059fa77c2f3f9ef06c45a94f28ac
Bruno Vicente <brunovicente.dev@gmail.com>
2023-09-13 14:44:16 -0300

    Put in specific folder

---
+++ Basics/index.py
@@ -0,0 +1,33 @@
+# How to print things in terminal --
+print("Hello world!!")
+
+# You can use "+" or "," to concatenate strings or "," to concatenate numbers --
+print("3+3: " + "6")
+print("4+4: ", 8)
+
+# *****************************************************************************************************************
+
+print(type(9.6)) #- it tells you what is the type of something (ex: int, float, string, etc)
+
+# *****************************************************************************************************************
+
+# To add use "+" -
+print("4+4=", 4+4)
+
+# To subtract use "-" -
+print("5-4=", 5-4)
+
+# To multiply use "*" -
+print("3x5=", 3*5)
+
+# To divide use "/" -
+print("4/2=", 4/2)
+
+# To power use "**" -
+print("2²=", 2**2)
+
+# To get the rest of the division use "%" -
+print("10%3=", 10%3)
+
+# To get only the int floor number of a division use "//" -
+print("3//2=", 3//2)
--- index.py
+++
@@ -1,3 +0,0 @@
-print("Hello world!!")
-print("3+3: " + "6")
-print("4+4: ", 8)

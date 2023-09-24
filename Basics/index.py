d7bfd88c5e1a233cf73f0b6fadc8c742b351cbdc
bruno <brunovicente.dev@gmail.com>
2023-09-24 14:23:34 -0300

    Updated code's visual

--- Basics/index.py
+++ Basics/index.py
@@ -7,27 +7,29 @@ print("4+4: ", 8)
 
 # *****************************************************************************************************************
 
-print(type(9.6)) #- it tells you what is the type of something (ex: int, float, string, etc)
+print(
+    type(9.6)
+)  # - it tells you what is the type of something (ex: int, float, string, etc)
 
 # *****************************************************************************************************************
 
 # To add use "+" -
-print("4+4=", 4+4)
+print("4+4=", 4 + 4)
 
 # To subtract use "-" -
-print("5-4=", 5-4)
+print("5-4=", 5 - 4)
 
 # To multiply use "*" -
-print("3x5=", 3*5)
+print("3x5=", 3 * 5)
 
 # To divide use "/" -
-print("4/2=", 4/2)
+print("4/2=", 4 / 2)
 
 # To power use "**" -
 print("2²=", 2**2)
 
 # To get the rest of the division use "%" -
-print("10%3=", 10%3)
+print("10%3=", 10 % 3)
 
 # To get only the int floor number of a division use "//" -
-print("3//2=", 3//2)
+print("3//2=", 3 // 2)

052f9d826d6797316572e810c4d0a58089c0c559
bruno <brunovicente.dev@gmail.com>
2023-10-12 14:33:11 -0300

    Added some comments

--- Basics/index.py
+++ Basics/index.py
@@ -8,28 +8,32 @@ print("4+4: ", 8)
 # *****************************************************************************************************************
 
 print(
+    # It tells you what is the type of something (ex: int, float, string, etc) --
     type(9.6)
-)  # - it tells you what is the type of something (ex: int, float, string, etc)
+)
 
 # *****************************************************************************************************************
 
+# "\n" makes it space a line --
+print("\nMath operations:\n")
+
 # To add use "+" -
-print("4+4=", 4 + 4)
+print("4+4 =", 4 + 4)
 
 # To subtract use "-" -
-print("5-4=", 5 - 4)
+print("5-4 =", 5 - 4)
 
 # To multiply use "*" -
-print("3x5=", 3 * 5)
+print("3x5 =", 3 * 5)
 
 # To divide use "/" -
-print("4/2=", 4 / 2)
+print("4/2 =", 4 / 2)
 
 # To power use "**" -
-print("2²=", 2**2)
+print("2² =", 2**2)
 
 # To get the rest of the division use "%" -
-print("10%3=", 10 % 3)
+print("10%3 =", 10 % 3)
 
 # To get only the int floor number of a division use "//" -
-print("3//2=", 3 // 2)
+print("5//2 =", 5 // 2)

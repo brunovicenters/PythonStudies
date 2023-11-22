1fe4e8d6eb594dbf69ea6053dde3a44774575d57
Bruno Vicente <brunovicente.dev@gmail.com>
2023-11-21 23:06:30 -0300

    First commit

---
+++ Strings/index.py
@@ -0,0 +1,12 @@
+# *****************************************************************************************************************
+
+#                                                                        Strings
+
+# *****************************************************************************************************************
+
+# You can use double quotes or single quotes to create strings --
+print("Hello world!!")
+print('Hello world!!')
+
+# And you can even use them together --
+print("She said 'Hello world!!'")

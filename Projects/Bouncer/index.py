a7dc0855167bca128d9072b7862a536dfd2402ad
bruno <brunovicente.dev@gmail.com>
2024-03-13 15:12:50 -0300

    Refactored

--- Projects/Bouncer/index.py
+++ Projects/Bouncer/index.py
@@ -2,10 +2,10 @@ age = input("How old are you: ")
 
 if age:
     age = int(age)
-    if age >= 18 and age < 21:
-        print("You can enter, but need a wristband!")
-    elif age >= 21:
+    if age >= 21:
         print("You're good to enter and drink!")
+    elif age >= 18:
+        print("You can enter, but need a wristband!")
     else:
         print("You can't enter!")
 else:

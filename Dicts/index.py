4ca34f9ec879d29ab1407103e479fd1c07779950
Bruno Vicente <brunovicente.dev@gmail.com>
2024-03-26 14:59:06 -0300

    Added iterating

--- Dicts/index.py
+++ Dicts/index.py
@@ -1,4 +1,4 @@
-# sourcery skip: collection-to-bool, de-morgan, equality-identity, remove-redundant-if, use-assigned-variable, while-to-for;for-index-replacement
+# sourcery skip: collection-to-bool, de-morgan, equality-identity, remove-redundant-if, use-assigned-variable, while-to-for, for-index-replacement, remove-dict-keys
 # *****************************************************************************************************************
 
 #                                                    DICTIONARY
@@ -13,6 +13,7 @@
 
 print("\nDICTIONARY:")
 
+# To create a dictionary you use {}, and you add items with keys and values
 cart_item = {"name": "Milk", "price": 3.99, "quantity": 5}
 print(f"\nCart item -> {cart_item}")
 print(f"Cart item name -> {cart_item['name']}")
@@ -37,10 +38,35 @@ for item in shopping_cart:
 
 # *****************************************************************************************************************
 
-#                                           Another way to create dict
+#                                          Another way to create dicts
 
 # *****************************************************************************************************************
 
 print("\nAnother way to create dict:")
 person = dict(name="Bruno", age=32, country="Brazil")
 print(person)
+
+# *****************************************************************************************************************
+
+#                                            ITERATING DICTIONARIES
+
+# *****************************************************************************************************************
+
+print("\nITERATING DICTIONARIES:")
+
+# You can iterate over dictionaries using .values(), .keys() or .items() method
+
+# .values() returns a list of all values
+print("\nValues ->")
+for value in person.values():
+    print(value)
+
+# .keys() returns a list of all keys
+print("\nKeys ->")
+for key in person.keys():
+    print(key)
+
+# .items() returns a list of tuples with keys and values
+print("\nItems ->")
+for key, value in person.items():
+    print(f"{key}: {value}")

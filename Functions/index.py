bcbc941db85ee49e604421e4a186013a8058f831
Bruno Vicente <brunovicente.dev@gmail.com>
2024-08-12 21:45:10 -0300

    Added functions

---
+++ Functions/index.py
@@ -0,0 +1,34 @@
+# sourcery skip: collection-to-bool, de-morgan, equality-identity, remove-redundant-if, use-assigned-variable, dict-assign-update-to-union, while-to-for, for-index-replacement, remove-dict-keys, unwrap-iterable-construction, remove-duplicate-set-key;
+# *****************************************************************************************************************
+
+#                                                  FUNCTIONS
+
+# *****************************************************************************************************************
+
+# *****************************************************************************************************************
+
+#                                               Functions basics
+
+# *****************************************************************************************************************
+
+print("\nFUNCTIONS:")
+
+
+# To define a function use def
+def say_hello():
+    print("\nHello World!\n")
+
+
+# To call a function use function_name()
+say_hello()
+
+
+# To define a function with parameters pass them inside the parentesis (you can define it's type if you want)
+def happy_birthday(name: str):
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print(f"Happy Birthday dear {name}!")
+    print("Happy Birthday to you!\n")
+
+
+happy_birthday("Tarsila")
